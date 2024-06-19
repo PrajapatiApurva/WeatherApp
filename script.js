11de8659c39451977307d3da61299bd17c1d02ac
apurva-6265 <apurva18092003@gmail.com>
2024-06-19 22:27:27 +0530

    Added some fields called Country and Feeling Temp

--- script.js
+++ script.js
@@ -10,10 +10,14 @@ document.getElementById('getWeatherBtn').addEventListener('click', function() {
             if (data.cod === 200) {
                 const weather = data.weather[0].description;
                 const temp = data.main.temp;
+                const feels_like = data.main.feels_like;
+                console.log(data,'/n',weather,'/n',temp);
                 document.getElementById('weatherResult').innerHTML = `
                     <h3>${data.name}</h3>
+                    <p>Country: ${data.sys.country}</p>
                     <p>Weather: ${weather}</p>
-                    <p>Temperature: ${temp} °C</p>
+                    <p>Current Temperature: ${temp} °C</p>
+                    <p>Temperature Feels Like: ${feels_like} °C</p>
                 `;
             } else {
                 document.getElementById('weatherResult').innerHTML = `<p>${data.message}</p>`;
